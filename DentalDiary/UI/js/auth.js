d6ae66f97f43633a2cec6771a7326bb171a562dc
Oleh Opalinskyi <oleh.opalinskyi@gamil.com>
2017-09-24 13:04:48 +0300

    add auth page

---
+++ DentalDiary/UI/js/auth.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+    $("#login").click(function () {
+        var login = $("input[name='user']");
+        var password = $("input[name='pass']");
+
+        $.ajax({
+            url: "http://localhost:50612/token",
+            method: "post",
+            data: {
+                grant_type: "password",
+                username: login.val(),
+                password: password.val()
+            },
+            error: function (jqXHR) {
+                alert(jqXHR.responseJSON.error_description);
+            },
+            success: function (data) {
+                console.log(data);
+                var tokenInfo = {
+                    'token': "Bearer " + data.access_token,
+                    'expire': new Date().valueOf() + (data.expires_in * 1000)
+                }
+                localStorage.setItem('token', JSON.stringify(tokenInfo));
+                window.location.replace("index.html");
+            }
+        });
+    });
+});
